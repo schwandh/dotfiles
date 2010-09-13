d685ba667eda77e17b7f054c16cf915d4a947f7b
Will Mernagh <wmernagh@gmail.com>
2010-09-13 17:42:57 -0400

    Add git completion script so tabbing works with git alias
    e.g.
    git co <TAB> will now work.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -22,4 +22,5 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status -s' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
