093c7501cc9d5b81f1fb42c3756b1d6f154d6f12
Will Mernagh <wmernagh@gmail.com>
2010-09-13 21:34:36 -0400

    Added an alias to list git aliases :)

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,6 +17,7 @@ alias gl='git pull --rebase --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias ga='git add'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
@@ -24,3 +25,4 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gs='git status -s' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias galias='alias|grep git'
